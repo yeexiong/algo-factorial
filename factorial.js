3d442314df29b315381dce8cccf3a46bf300b421
yeexiong <yee.l.xiong@gmail.com>
2025-11-08 13:03:53 -0600

    Yee's first assignment

--- factorial.js
+++ factorial.js
@@ -1,5 +1,17 @@
 function factorial(num) {
-  return 0;
+  let input = num;
+  
+  if (input < 0){
+    return 1
+
+  } else {
+    let result = 1;
+
+    for (let i = input; i >= 1; i--) {
+      result *= i;
+    }
+  return result;
+  }
 }
 
 module.exports = factorial;
